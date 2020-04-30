d5f01a41cf96de41f2dfb357de228889e6ce2327
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-04-30 23:02:53 +0200

    Poke cache prefix to clear cache

--- style/css.php
+++ style/css.php
@@ -1,7 +1,7 @@
 <?php
 
 /* change this prefix when you want to make sure cache is cleared */
-$apc_cache_prefix = '2019-08-15-b';
+$apc_cache_prefix = '2020-04-30-b';
 
 $site = urlencode ($_GET['site']);
 

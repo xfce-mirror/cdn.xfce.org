15d4291c302f13ac8000df5829f807843ae422df
Alexander Schwinn <alexxcons@xfce.org>
2022-12-14 14:42:38 +0100

    clear cache

--- style/css.php
+++ style/css.php
@@ -1,7 +1,7 @@
 <?php
 
 /* change this prefix when you want to make sure cache is cleared */
-$apc_cache_prefix = '2020-10-19-a';
+$apc_cache_prefix = '2022-12-14-a';
 
 $site = urlencode ($_GET['site']);
 

ce0efcc6c822d04efc4af5a368403d16dc98c8bf
Alexander Schwinn <alexxcons@xfce.org>
2024-12-05 19:07:26 +0100

    Update Slider

--- style/css.php
+++ style/css.php
@@ -1,7 +1,7 @@
 <?php
 
 /* change this prefix when you want to make sure cache is cleared */
-$apc_cache_prefix = '2024-12-05-a';
+$apc_cache_prefix = '2024-12-05-b';
 
 $site = urlencode ($_GET['site']);
 

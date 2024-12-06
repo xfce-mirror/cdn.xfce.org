7800c630cfd84ae3fd67f8e13e723f25549339c3
Alexander Schwinn <alexxcons@xfce.org>
2024-12-06 08:14:11 +0100

    bump apc_cache_prefix

--- style/css.php
+++ style/css.php
@@ -1,7 +1,7 @@
 <?php
 
 /* change this prefix when you want to make sure cache is cleared */
-$apc_cache_prefix = '2024-12-05-b';
+$apc_cache_prefix = '2024-12-06-a';
 
 $site = urlencode ($_GET['site']);
 

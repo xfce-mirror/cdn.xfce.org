9296b0a5daaaabccd4620145aa7c18cfdffa8415
Alexander Schwinn <alexxcons@xfce.org>
2024-12-14 17:42:27 +0100

    Add screenshots for 4.20 tour

--- style/css.php
+++ style/css.php
@@ -1,7 +1,7 @@
 <?php
 
 /* change this prefix when you want to make sure cache is cleared */
-$apc_cache_prefix = '2024-12-09-a';
+$apc_cache_prefix = '2024-12-10-a';
 
 $site = urlencode ($_GET['site']);
 
